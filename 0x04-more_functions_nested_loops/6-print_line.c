b39f1dd2d18fa9c2206ba6c5f41b63599310bf39
Diverse2 <ntwalie83@gmail.com>
2023-08-28 16:50:02 -0700

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,18 @@
 
 void print_line(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+       	else
+	{
+		int i;
 
-	_putchar(0) '\n';
-	_putchar(2) '\n';
-	_putchar(10) '\n';
-	_putchar(-4) || ('\n');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
